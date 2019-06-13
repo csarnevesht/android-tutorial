c0242226a3967ffda7e687ca30878c59204810f0
csarnevesht <carops67@gmail.com>
2019-06-13 07:47:08 -0700

    add more trophies

--- tutorial-projects/recyclerview/app/src/main/java/com/example/androidtutorial/TrophiesActivity.java
+++ tutorial-projects/recyclerview/app/src/main/java/com/example/androidtutorial/TrophiesActivity.java
@@ -51,6 +51,18 @@ public class TrophiesActivity extends AppCompatActivity {
         trophies.add(trophy);
         trophy = new Trophy("Swimming - Anthony Lao - 2017", "Don't let Anthony fool you because he is a sophomore.  He has strong arms and legs and he will not be defated. Good job Mr. Lao!");
         trophies.add(trophy);
+        trophy = new Trophy("Swimming - Mark Levin - 2017]6", "Swimming: Falcons pull together for smooth start to season");
+        trophies.add(trophy);
+        trophy = new Trophy("Swimming - Samantha Marin - 2015", "Swimming: Falcons pull together for smooth start to season");
+        trophies.add(trophy);
+        trophy = new Trophy("Football - Tushar Sri - 2014", "Top performer");
+        trophies.add(trophy);
+        trophy = new Trophy("Football - Christoher Lin - 2018", "Everyone on the team stepped up and held their ground in almost every event");
+        trophies.add(trophy);
+        trophy = new Trophy("Tennis - Theresa Mondo - 2017", "The team is much more balanced");
+        trophies.add(trophy);
+        trophy = new Trophy("Tennis - Nicolas Smith - 2015", "Top performer");
+        trophies.add(trophy);
         adapter.notifyDataSetChanged();
     }
 }
